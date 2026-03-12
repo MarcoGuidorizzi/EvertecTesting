f7a809d89808d5e09c234c7f29c5256310095e3d
MARCO ANTONIO GUIDORIZZI COUTINHO DE SOUZA <62554313+MarcoGuidorizzi@users.noreply.github.com>
2026-03-11 21:41:42 -0300

    Refactor PontoTuristicoModel

--- PontoTuristicoBackEnd/PontoTuristico/Models/PontoTuristicoModel.cs
+++ PontoTuristicoBackEnd/PontoTuristico/Models/PontoTuristicoModel.cs
@@ -4,11 +4,6 @@ namespace PontoTuristicoApp.Models
 {
     public class PontoTuristicoModel
     {
-        //usei a [key] para já identificar que o Id é a chave primária da minha tabela, ou seja,
-        //é o campo que vai identificar unicamente cada registro da minha tabela.
-        //O [required] para garantir que os campos sejam preenchidos, ou seja, que não sejam nulos.
-        //O [maxLength] para limitar a quantidade de caracteres que podem ser inseridos na descrição,
-        //para evitar que o usuário insira uma descrição maior que 100 caracteres, como foi solicitado na descrição do próprio teste.
         [Key]
         public int Id { get; set; }
         [Required(ErrorMessage = "Nome é um campo obrigatório.")]
